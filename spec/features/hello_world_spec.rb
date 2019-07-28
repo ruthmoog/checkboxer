88aa73811531bab1a406094b3b352dafa51c444f
Ruth Baker <ruthmoog@gmail.com>
2019-07-28 21:49:43 +0100

    Refactor tests

--- spec/features/hello_world_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-feature 'Hello, world!' do
-  scenario 'index displays hello world' do
-    visit '/'
-    expect(page).to have_content('Hello, world!')
-  end
-end
--- spec/features/user_submits_empty_form_spec.rb
+++ spec/features/user_submits_empty_form_spec.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-feature 'user submits empty form' do
+feature 'user submits empty template request' do
   scenario 'user clicks submit' do
     visit '/'
+    click_link 'Checklist'
     click_button 'Submit'
     expect(page).to have_content('Dear author,')
   end
