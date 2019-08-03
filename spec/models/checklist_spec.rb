4c1a57d8d7e25523a40eecfd697e9a3935949008
Ruth Baker <ruthmoog@gmail.com>
2019-08-03 22:51:17 +0100

    Refactor test for checklist model

--- spec/models/checklist_spec.rb
+++ spec/models/checklist_spec.rb
@@ -4,9 +4,13 @@ require 'check'
 
 RSpec.describe 'check' do
   it 'returns a collection of checks' do
-    expected_check = Check.new('request a change', 'Please complete request a change.')
-    expect(Check.generate).to be_an(Array)
-    expect(Check.generate[0].requirement).to eq(expected_check.requirement)
-    expect(Check.generate[0].request).to eq(expected_check.request)
+    expected_check = Check.new(
+      'request a change',
+      'Please complete request a change.'
+    )
+    checklist = Check.generate
+    expect(checklist).to be_an(Array)
+    expect(checklist[0].requirement).to eq(expected_check.requirement)
+    expect(checklist[0].request).to eq(expected_check.request)
   end
 end
